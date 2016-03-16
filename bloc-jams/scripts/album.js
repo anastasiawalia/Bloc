25d24f99c11b4a6890f1803fcb31ea56d1ef17d6
anastasia <anastasiawalia@gmail.com>
2016-03-16 15:46:16 -0500

    30 jQuery: Collection and Album Views

--- bloc-jams/scripts/album.js
+++ bloc-jams/scripts/album.js
@@ -55,29 +55,31 @@ var albumMusk = {
       + '</tr>'
       ;
  
-     return template;
+     return $(template);
  };
 
+var setCurrentAlbum = function(album) {
 //Select elements you want to populate with text dynamically 
-var albumTitle = document.getElementsByClassName('album-view-title')[0];
-var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-var albumImage = document.getElementsByClassName('album-cover-art')[0];
-var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+     var $albumTitle = $('.album-view-title');
+     var $albumArtist = $('.album-view-artist');
+     var $albumReleaseInfo = $('.album-view-release-info');
+     var $albumImage = $('.album-cover-art');
+     var $albumSongList = $('.album-view-song-list');
 
- var setCurrentAlbum = function(album) {
+
      // assign values to each part of the album (text, images )
-     albumTitle.firstChild.nodeValue = album.name;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
+     $albumTitle.text(album.title);
+     $albumArtist.text(album.artist);
+     $albumReleaseInfo.text(album.year + ' ' + album.label);
+     $albumImage.attr('src', album.albumArtUrl);
  
      // Clear contents of album song list container
-     albumSongList.innerHTML = '';
+    $albumSongList.empty();
  
      // build list of songs from album js object
      for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         $albumSongList.append($newRow);
      }
  };
 
