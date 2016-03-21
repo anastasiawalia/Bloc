324b6a79cfc7ed6e704c47f73b5a25960cb8e00f
anastasia <anastasiawalia@gmail.com>
2016-03-21 11:56:25 -0500

    Lesson 27 Submit DOM Scripting: Play/Pause Part 2

--- bloc-jams/scripts/album.js
+++ bloc-jams/scripts/album.js
@@ -131,3 +131,38 @@ var updatePlayerBarSong = function() {
              currentSongFromAlbum = null;
          }
 
+var findParentByClassName = function(element, targetClass) {
+    if (element) {
+        var currentParent = element.parentElement;
+        
+        if (currentParent) {
+            while (currentParent.className && currentParent.className != targetClass) {
+                currentParent = currentParent.parentElement;
+            }
+        if (currentParent.className === targetClass) {
+            return currentParent;
+        } else {
+            alert("No parent with class name found");
+        } else {
+            alert("No parent found");
+        }
+    };
+      
+
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
